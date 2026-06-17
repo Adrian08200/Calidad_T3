761cdf4cfc7c3fcb0766988eeeaa89f1541031c9
Adrian08200 <adrian08_mendezramirez@hotmail.com>
2026-06-17 14:50:47 -0500

    Update test_sistema.py
    Removed comments from the test case for clarity.

--- test_sistema.py
+++ test_sistema.py
@@ -1,11 +1,9 @@
-# test_sistema.py
 import unittest
 from sistema import CarteraInversion, ProyectoMinero
 
 class TestCarteraMinera(unittest.TestCase):
     
     def test_suma_proyectos_nuevos_debe_ser_3794(self):
-        # 1. Arrange (Preparar los datos de la lectura)
         cartera = CarteraInversion()
         
         cartera.agregar_proyecto(ProyectoMinero("Reposición Ferrobamba", 1753, "Nuevo"))
@@ -15,10 +13,8 @@ class TestCarteraMinera(unittest.TestCase):
         cartera.agregar_proyecto(ProyectoMinero("Ampliación Huancapetí", 345, "Nuevo"))
         cartera.agregar_proyecto(ProyectoMinero("Ampliación Huachocolpa", 167, "Nuevo"))
 
-        # 2. Act (Ejecutar la función que queremos probar)
         resultado_calculado = cartera.calcular_total_por_tipo("Nuevo")
 
-        # 3. Assert (Verificar si el resultado es igual al esperado de US$ 3794M)
         self.assertEqual(resultado_calculado, 3794)
 
 if __name__ == '__main__':
